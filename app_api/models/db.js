4a39b383ea116f593cf5a92213a130ec52cc6c7c
Mario Frederick <Mario_frederick@snhu.edu>
2026-03-30 01:19:20 -0400

    Module 5 complete - API routes, controllers, and JSON output

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -16,23 +16,24 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
 
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close(() => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
+const gracefulShutdown = async (msg) => {
+  await mongoose.connection.close();
+  console.log(`Mongoose disconnected through ${msg}`);
 };
 
-process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart', () => process.kill(process.pid, 'SIGUSR2'));
+process.once('SIGUSR2', async () => {
+  await gracefulShutdown('nodemon restart');
+  process.kill(process.pid, 'SIGUSR2');
 });
 
-process.on('SIGINT', () => {
-  gracefulShutdown('app termination', () => process.exit(0));
+process.on('SIGINT', async () => {
+  await gracefulShutdown('app termination');
+  process.exit(0);
 });
 
-process.on('SIGTERM', () => {
-  gracefulShutdown('app termination', () => process.exit(0));
+process.on('SIGTERM', async () => {
+  await gracefulShutdown('app termination');
+  process.exit(0);
 });
 
 module.exports = mongoose;
--- app_api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const tripsRouter = require('./trips');
-
-router.use('/', tripsRouter);
-
-module.exports = router;
